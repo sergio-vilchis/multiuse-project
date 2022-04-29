60fa02ac1b4c6851a914cf0b3469ccc1f5a98df1
Sergio Vilchis Quintero <s.vilchisq@gmail.com>
2022-04-29 10:39:14 -0500

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function (clientApp) {
+    var contentTypeHtml = "text/html";
+    var mainFolder = "client/front-end";
+    const fs = require('fs').promises;
+
+    clientApp.get('/', (_req, res) => {
+        fs.readFile(mainFolder+"/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", contentTypeHtml);
+            res.writeHead(200);
+            res.end(contents);
+        })
+    });
+};
