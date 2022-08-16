d496f917033088955e2871b0bcab5f2edae9a47e
Sergio Vilchis Quintero <s.vilchisq@gmail.com>
2022-08-16 11:09:55 -0500

    Update to use only one port

--- app.js
+++ app.js
@@ -5,8 +5,6 @@
 
 const http = require('http');
 var express = require('express'),
-	apiApp = express(),
-	port = 5050,
 	clientApp = express(),
 	portClient = 5000,
 	bodyParser = require('body-parser'); //Library for API requests (server side)
@@ -16,18 +14,11 @@ var express = require('express'),
 	clientApp.use(bodyParser.json());
 	var clientServer = require('./client/index.js');
 	clientServer(clientApp);
+	//API SERVER
+	var routes = require('./server/login'); //importing login route to server
+	routes(clientApp); //register the route
 	var httpServerClient = http.createServer(clientApp);
 	httpServerClient.listen(portClient);
-	console.log('Client app started on: ' + portClient);
-
-	//API Server
-	apiApp.use(bodyParser.urlencoded({ extended: true }));
-	apiApp.use(bodyParser.json());
-	var routes = require('./server/login'); //importing login route to server
-	routes(apiApp); //register the route
-	var httpServer = http.createServer(apiApp);
-	httpServer.listen(port);
-	console.log('RESTful API server started on: ' + port);
-
-	module.exports.apiApp = apiApp;
+	console.log('App started on: ' + portClient);
 	module.exports.clientApp = clientApp;
+	module.exports.apiApp = clientApp;
