88b04d9c2dc871d114dc65c607c621e91d682a85
Sergio Vilchis Quintero <s.vilchisq@gmail.com>
2022-08-16 19:06:47 -0500

    Main test file

--- test/clientTests/appTest.js
+++ test/clientTests/appTest.js
@@ -2,13 +2,6 @@ const { assert } = require('chai');
 const request = require('supertest');
 const app = require('../../app')
 
-before(done => {
-  app.initServer
-  setTimeout(function() {
-    done();
-  }, 5000);
-});
-
 describe('GET root 200 Client', () => {
   it('Should answer with 200',  () => {
      return request(app.clientApp)
